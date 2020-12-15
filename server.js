b0ba2a3282176d263972ea91cfb05bf6afe2ac5a
Sally Perez <seattlesally1@gmail.com>
2020-12-14 19:22:18 -0700

    added initial express calls to handle data parsing

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Note Taker app
 // Dependencies
 // ===========================================================
 var express = require("express");
@@ -5,10 +6,17 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// make public folder available
+app.use(express.static('public'))
+
 // Routes
 // ===========================================================
 app.get("/", function(req, res) {
-    res.send("Welcome to the Note Taker!");
+    res.send("Welcome... to the Note Taker!");
   });
 
   
